1476642a063339568f0b85ee9334ff87dbf8a5c0
clairegreiner <clairegreiner@gmail.com>
2016-08-07 14:59:25 -0400

    added frame score  array

--- src/com/day3/Bowling2.java
+++ src/com/day3/Bowling2.java
@@ -7,8 +7,8 @@ public class Bowling2 {
 		int initialPins = 10;
 		int numFrames = 10;
 		int throwsPerFrame = 2;
+		int frames[] = new int[10];
 		int runningScore = 0;
-		
 
 		// random throws 1 and 2
 
@@ -23,34 +23,38 @@ public class Bowling2 {
 			int pinsRemaining = 0;
 			int pinsLeft = 0;
 			int frameScore = 0;
-			
-			System.out.println("Frame #" + (i+1));
+
+			System.out.println("Frame #" + (i + 1));
 			// roll 1 mathiness
 
-			pinsStruck = (int) (Math.random() *11); // roll 1
+			pinsStruck = (int) (Math.random() * 11); // roll 1
 			System.out.println("Roll 1: " + pinsStruck);
-			pinsSpare = (int) ((10 - pinsStruck) - (Math.random() * (11 - pinsStruck))); 
-														//does this make sense?
-			
-																										// roll
-																							// 2
-																							// math
-																							// random
-																							// of
-																							// remaining
-																							// pins
+			pinsSpare = (int) ((10 - pinsStruck) - (Math.random() * (11 - pinsStruck)));
+			// does this make sense?
+
+			// roll
+			// 2
+			// math
+			// random
+			// of
+			// remaining
+			// pins
 			System.out.println("Roll 2: " + pinsSpare);
-			
+
 			frameScore = pinsStruck + pinsSpare;
 			runningScore = runningScore + frameScore;
+			frames[i] = frameScore;
 			System.out.println("Frame score " + frameScore); // nest further
 																// to record
 																// both
-			System.out.println("Running score is " + runningScore );											// rolls/
-				
-				
+			System.out.println("Running score is " + runningScore);
+
+			// rolls/
 
 		}
+
+		for (int i = 0; i < frames.length; i++)
+			System.out.println(frames[i]);
+
 	}
 }
-
